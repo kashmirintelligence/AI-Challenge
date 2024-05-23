b4eafed1b856492e672eef9be0f61898c3a06e29
SamPlvs <samyaktukra@gmail.com>
2024-05-23 08:09:14 +0100

    updated challenge

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 Welcome! you are a super star for making it here. This is your time to shine, an opportunity to show off your skills, understanding and more importantly coding abilities 😉. So relax, grab some coffee / whiskey (depending on time of day) and start developing.
 
-As a reminder our interview process is depicted in the following:
-
-
-## Repository Structure ⛩️
-
-
 ## Setup 💿
 
+1. Clone this repository to your local machine.
+2. Install the required Python libraries:
+
+```shell
+pip install -r requirements.txt
+```
+Please feel free to install any other repositories of your choice. For any deep learning implementations, please use [`PyTorch`](https://pytorch.org/)
 
 ## Challenge Overview 💪
 There are 3 challenges, to complete. Each one has options, so pick one that is most suitable to your domain expertise.
---
+++ src/utils/download_yfinance_data.py
@@ -0,0 +1,52 @@
+import yfinance as yf
+import pandas as pd
+import argparse
+from datetime import datetime, timedelta
+
+def download_stock_data(ticker, start_date, end_date, interval='5m'):
+    """
+    Download stock data from Yahoo Finance.
+
+    :param ticker: str, Stock ticker symbol.
+    :param start_date: str, Start date in the format 'YYYY-MM-DD'.
+    :param end_date: str, End date in the format 'YYYY-MM-DD'.
+    :param interval: str, Data interval. Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo].
+    :return: pd.DataFrame, Stock data.
+    """
+    stock_data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    return stock_data
+
+def save_to_csv(dataframe, filename):
+    """
+    Save DataFrame to a CSV file.
+
+    :param dataframe: pd.DataFrame, Data to save.
+    :param filename: str, Path to the file where data will be saved.
+    """
+    dataframe.to_csv(filename, index=True)
+    print(f"Data saved to {filename}")
+
+if __name__ == "__main__":
+    # Default dates: from 5 days ago to yesterday
+    end_date_default = datetime.today() - timedelta(days=1)
+    start_date_default = end_date_default - timedelta(days=5)
+
+    parser = argparse.ArgumentParser(description='Download stock data from Yahoo Finance.')
+    parser.add_argument('ticker', type=str, help='Stock ticker symbol')
+    parser.add_argument('--start_date', type=str, default=start_date_default.strftime('%Y-%m-%d'), help='Start date in the format YYYY-MM-DD (default: 5 days ago)')
+    parser.add_argument('--end_date', type=str, default=end_date_default.strftime('%Y-%m-%d'), help='End date in the format YYYY-MM-DD (default: yesterday)')
+    parser.add_argument('--interval', type=str, default='5m', help='Data interval (default: 5m)')
+
+    args = parser.parse_args()
+
+    ticker = args.ticker
+    start_date = args.start_date
+    end_date = args.end_date
+    interval = args.interval
+
+    # Download stock data
+    stock_data = download_stock_data(ticker, start_date, end_date, interval)
+
+    # Save to CSV
+    filename = f"{ticker}_stock_data.csv"
+    save_to_csv(stock_data, filename)
